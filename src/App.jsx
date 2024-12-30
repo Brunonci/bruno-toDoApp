7190785fba4af1634843af5d06c3a39101f096c0
Bruno Cafaro <brunonci@hotmail.com>
2024-12-30 11:10:45 -0300

    temas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [theme, setTheme] = useState(false);
 
   const addTask = () => {
     if (newTask.trim() !== '') {
@@ -12,32 +13,63 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.querySelector('body').classList.add('dark');
+    } else {
+      document.querySelector('body').classList.remove('dark');
+    }
+  }, [theme]);
+
   const deleteTask = (index) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
   };
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <div className="App flex flex-col items-center width-100 h-screen justify-center ">
-      <h1 className="text-xl font-bold">To-Do List</h1>
-      <div>
-        <input
-          type="text"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          placeholder="Add a new task"
-        />
-        <button onClick={addTask}>Add</button>
+    <div className="dark:bg-gray-800 bg-gray-100 h-screen">
+      <button
+        className="flex justify-end bg-blue-400 text-black dark:bg-gray-700 dark:text-white p-2 rounded-md dark:hover:bg-gray-300 "
+        onClick={toggleTheme}>
+        {theme ? 'Tema Claro' : 'Tema Oscuro'}
+      </button>
+      <h1 className="text-4xl font-bold flex justify-center p-4 dark:text-white ">
+        To-Do List
+      </h1>
+
+      <div className="flex flex-col items-center p-4">
+        <div>
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Add a new task"
+          />
+          <button
+            onClick={addTask}
+            className="bg-blue-400 text-black dark:bg-gray-700 dark:text-white p-2 rounded-md m-2 hover:bg-gray-300">
+            Add
+          </button>
+        </div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}{' '}
+              <button
+                className="bg-blue-400 text-black dark:bg-gray-700 dark:text-white p-2 rounded-md m-2 hover:bg-gray-300"
+                onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task} <button onClick={() => deleteTask(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
-}
+};
 
 export default App;
